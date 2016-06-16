f115da89c8534b0c4503ca03fc36a108f4c195ef
vinnie garcia <vingarcia@paypal.com>
2016-06-16 11:09:57 -0700

    added koa-body-parser and env variable parsing for production

--- server/config.js
+++ server/config.js
@@ -7,6 +7,18 @@ const env = process.env.NODE_ENV;
 if (!env || env.indexOf('dev') === 0) {
     Object.assign(config, require('../config/development.json'), { dev: true });
     config.paypal = require('../config/paypal.json');
+} else {
+    const client_id = process.env.CLIENT_ID,
+        secret = process.env.CLIENT_SECRET,
+        environment = process.env.PAYPAL_ENV,
+        merchant_id = process.env.MERCHANT_ID;
+    
+    config.paypal = {
+        client_id,
+        secret,
+        environment,
+        merchant_id
+    };
 }
 
 export default config;
